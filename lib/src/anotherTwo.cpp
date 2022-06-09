e4d4df4f19395e7bf80e3aaa2a42c54897342559
Tom Mettam <tom@caspertech.co.uk>
2022-06-09 11:02:12 +0100

    anotherTwo

---
+++ lib/src/anotherTwo.cpp
@@ -0,0 +1,8 @@
+#include "anotherTwo.h"
+
+#include <iostream>
+
+void anotherTwo::print()
+{
+	std::cout << "Class AnotherTwo is active" << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
 #include "src/two.h"
+#include "lib/src/anotherTwo.h"
 
 int main()
 {
 	auto ptr = std::make_unique<two>();
 	ptr->print();
+
+	auto ptr2 = std::make_unique<anotherTwo>();
+	ptr2->print();
 	return 0;
 }
